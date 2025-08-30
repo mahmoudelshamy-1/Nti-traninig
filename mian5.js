53e67a7b4d9603ccad99875f775f0a66db582c37
mahmoudelshamy-1 <mahmoudelshamy449@gmail.com>
2025-08-30 03:19:38 +0300

    day5

---
+++ mian5.js
@@ -0,0 +1,40 @@
+  document.getElementById("btn").onclick = function myFunction(){
+    console.log("Button clicked")
+
+  }
+
+ const myTimeout = setTimeout(myGreeting, 3000);
+
+function myGreeting() {
+  alert ("welcome")
+}
+
+const element = document.getElementById("he");
+setInterval(function() {console.log  ("Hello")}, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function changeToRed() {
+    container.style.backgroundColor = 'red';
+}
