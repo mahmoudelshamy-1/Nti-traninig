e3196dbbc7e1b1b27879735950c1f33835971b71
mahmoudelshamy-1 <mahmoudelshamy449@gmail.com>
2025-08-30 03:19:13 +0300

    day4

---
+++ main.js
@@ -0,0 +1,20 @@
+var myname ="elshamy"
+ console.log(myname);
+ var gpa =3
+console.log((gpa));
+
+function yourresult(Name) {
+  console.log(`mahmoudelshamy ${Name}`);
+}
+yourresult("hello");
+
+
+
+ let GPA =2
+
+if (GPA>3){
+    console.log("Success")
+} 
+else{
+    console.log("failed")
+}
